1e58619bc62caffdd67477b747e271cf9736f7a1
Khaled <>
2023-02-10 11:33:55 +0100

    Initial project setup

---
+++ MyPodcast/MyPodcast/MyPodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPodcastApp.swift
+//  MyPodcast
+//
+//  Created by khaled on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyPodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
