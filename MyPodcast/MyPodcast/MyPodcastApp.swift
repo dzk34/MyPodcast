7d2fd13f28a59e60e7248b0066d22d68707f7e36
dzk34 <>
2024-02-16 18:08:16 +0000

    xcode project setup

---
+++ MyPodcast/MyPodcast/MyPodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPodcastApp.swift
+//  MyPodcast
+//
+//  Created by khaledus on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyPodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
